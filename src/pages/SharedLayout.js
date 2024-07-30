5e256efbeeaacd42730f1710dd382ffa9446c6aa
PetrB <petrbednarski@gmail.com>
2024-07-30 13:04:30 +0200

    Update SharedLayout.js

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,13 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 
 const SharedLayout = () => {
   return (
-    <section>
-      <Navbar />
+    <section className="shared-layout">
       <Outlet />
-      <Footer />
     </section>
   );
 };
