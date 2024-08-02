3a001435c70105a0c7c8640230e8fd27cc4e910a
pettik <petrbednarski@gmail.com>
2024-08-02 21:24:36 +0200

    new updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,13 @@ import './Home.css';
 import { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
+import {
+  FaSearch,
+  FaInfoCircle,
+  FaUserFriends,
+  FaSyncAlt,
+} from 'react-icons/fa';
+
 import { FiGithub } from 'react-icons/fi';
 
 const Home = () => {
@@ -15,6 +22,41 @@ const Home = () => {
     'aktualizovaná',
     'efektivní',
   ];
+
+  const benefits = [
+    {
+      icon: <FaSearch />, // Ikona pro rychlé vyhledávání
+      title: 'Rychlé vyhledávání',
+      description:
+        'Najděte GitHub účty během několika vteřin pomocí našeho výkonného vyhledávacího nástroje.',
+    },
+    {
+      icon: <FaInfoCircle />, // Ikona pro detailní informace
+      title: 'Detailní profily',
+      description:
+        'Získejte přehledné a detailní informace o jednotlivých uživatelích, včetně jejich projektů a příspěvků.',
+    },
+    {
+      icon: <FaUserFriends />, // Ikona pro uživatelsky přívětivé rozhraní
+      title: 'Jednoduché a intuitivní',
+      description:
+        'Uživatelsky přívětivé rozhraní, které je snadné používat pro všechny úrovně zkušeností.',
+    },
+    {
+      icon: <FaSyncAlt />, // Ikona pro aktualizované údaje
+      title: 'Aktualizované údaje',
+      description:
+        'Vždy aktuální informace přímo z GitHub API, abyste měli přístup k nejnovějším údajům.',
+    },
+  ];
+
+  const BenefitItem = ({ icon, title, description }) => (
+    <div className="benefit-item">
+      <div className="benefit-icon">{icon}</div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 
   const getRandomTitle = useCallback(() => {
     const randomIndex = Math.floor(Math.random() * appProperties.length);
@@ -30,25 +72,40 @@ const Home = () => {
 
   return (
     <main className="container">
+      {/* HERO SECTION */}
       <section className="hero">
-        <div className="logo-container">
-          <FaMagnifyingGlass className="magnifying__icon" />
-          <FiGithub className="gihub__center__icon" />
+        <div className="hero__container">
+          <div className="logo-container">
+            <FaMagnifyingGlass className="magnifying__icon" />
+            <FiGithub className="gihub__center__icon" />
+          </div>
+          <h1>
+            Aplikace <span>GithubUserFinder</span>
+          </h1>
+          <h3>
+            <span>{title}</span> aplikace pro vyhledávání Github uživatelů.
+          </h3>
+          <button className="btn__home">
+            <Link to="/github-profiles">
+              <span>Vyhledat uživatele</span>
+              <FaMagnifyingGlass className="btn__magnifying__icon" />
+            </Link>
+          </button>
         </div>
-        <h1>
-          Aplikace <span>GithubUserFinder</span>
-        </h1>
-        <h3>
-          <span>{title}</span> aplikace pro vyhledávání Github uživatelů.
-        </h3>
-        <button className="btn__home">
-          <Link to="/github-profiles">
-            <span>Vyhledat uživatele</span>
-            <FaMagnifyingGlass className="btn__magnifying__icon" />
-          </Link>
-        </button>
       </section>
-      <section className="benefits__section"></section>
+      {/* BENEFITS SECTION */}
+      <section className="benefits__section">
+        <div className="benefits__container">
+          {benefits.map((benefit, index) => (
+            <BenefitItem
+              key={index}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
