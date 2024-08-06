538229a967a78afe2b8fc46e46409c7fb29b8e51
PetrB <petrbednarski@gmail.com>
2024-08-06 09:38:20 +0200

    Update GitHubFinder.js

--- src/pages/GitHubFinder.js
+++ src/pages/GitHubFinder.js
@@ -1,7 +1,34 @@
+import {useState, useEffect} from "react"
+
 const GitHubFinder = () => {
+  const url = "https://api.github.com/users/pettik";
+  const [loading, setLoading] = useState(true);
+  const [login, setLogin] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect( () => {
+    fetch(url)
+      .then( (response) => response.json() )
+      .then( (data) =>{ 
+        console.log(data);
+        setLogin(data.login);
+        setLogin(data.login);
+        setLoading(false);
+      } )
+  })
+
+  if (loading) {
+    return <h2>Načítání stránky...</h2>
+  }
+
+
+  
   return (
     <section className="container">
       <h1>GithubUserFinder</h1>
+
+      <h2>{login}</h2>
+      <p>{name}</p>
     </section>
   );
 };
