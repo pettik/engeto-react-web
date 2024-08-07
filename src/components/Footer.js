cf828e034ecaa01076b4bcd242547ff1aa6d3fd0
PetrB <petrbednarski@gmail.com>
2024-08-07 07:11:54 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,58 @@
 import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FaDiscord, FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa6';
+
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer__container">
+        <ul>
+          <li> 
+            <Link to="/" className="logo">
+              <FaGithub className="logo-icon" /> Github<span>UserFinder</span>
+            </Link>
+        </li>
+        <li>
+          <ul className="footer__links">
+            <li>
+            <Link to="/">
+              Domů
+            </Link>
+            </li>
+            <li>
+            <Link to="/github-profiles">
+              GithubUserFinder App
+            </Link>
+            </li>
+            <li>
+            <Link to="/about">
+              O projektu
+            </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <ul className="footer__social">
+            <li>
+              <Link to="/">
+                <FaDiscord />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <FaGithubSquare />
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <FaLinkedin />
+              </Link>
+            </li>
+          </ul>
+        </li>
+        </ul>
         <p>2024 &copy; Bednarski P.</p>
       </div>
     </footer>
