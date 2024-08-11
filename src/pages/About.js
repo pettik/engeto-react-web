9e2917036e9c80a52d155fcd1b3fd13ea2ce5bf9
PetrB <petrbednarski@gmail.com>
2024-08-11 22:23:50 +0200

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,16 @@
 const About = () => {
   return (
     <section className="container">
-      <p>Tento projekt vznikl jako bla bla...</p>
+      <h2>O projektu GithubUserFinder</h2>
+      <p>
+        Tento projekt vznikl jako závěrečný úkol v rámci kurzu "React" pořádaného společností Engeto. Cílem projektu je demonstrovat schopnost vytvářet vícestránkové webové aplikace pomocí frameworku React, a to s využitím moderních vývojářských praktik a technik.
+      </p>
+      <p>
+        Aplikace GithubUserFinder umožňuje vyhledávat uživatele na GitHubu a zobrazovat o nich základní informace, které jsou získány z veřejného GitHub API. Tento projekt je navržen tak, aby zlepšil schopnost studentů pracovat s externími API, spravovat stav aplikace a vytvářet dynamické uživatelské rozhraní.
+      </p>
+      <p>
+        Projekt rovněž slouží jako praktická ukázka využití konceptů jako jsou komponenty, stavové řízení, a práce s datovými toky v rámci moderní aplikace. Doufáme, že vám tento projekt pomůže pochopit a osvojit si klíčové aspekty vývoje webových aplikací v Reactu.
+      </p>
     </section>
   );
 };
