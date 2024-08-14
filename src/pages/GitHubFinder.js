f02498a17c59aba2902681d30d0693c454da0007
pettik <petrbednarski@gmail.com>
2024-08-14 18:44:55 +0200

    new commit

--- src/pages/GitHubFinder.js
+++ src/pages/GitHubFinder.js
@@ -6,6 +6,7 @@ const GitHubFinder = () => {
   const [login, setLogin] = useState('');
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSearch = event => {
     event.preventDefault();
@@ -15,9 +16,15 @@ const GitHubFinder = () => {
 
     const url = `https://api.github.com/users/${username}`;
     setLoading(true);
+    setError(false);
 
     fetch(url)
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('User not found');
+        }
+        return response.json();
+      })
       .then(data => {
         console.log(data);
         setLogin(data.login);
@@ -27,6 +34,7 @@ const GitHubFinder = () => {
       .catch(error => {
         console.error('Error fetching data:', error);
         setLoading(false);
+        setError(true);
       });
   };
 
@@ -35,17 +43,22 @@ const GitHubFinder = () => {
       <div className="container__app">
         <h1>GithubUserFinder</h1>
         <form onSubmit={handleSearch}>
-          <input
-            type="text"
-            placeholder="Zadejte GitHub uživatelské jméno"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
+          <div className="github__heading__wrapper">
+            <span className="github__heading">github.com/</span>
+            <input
+              type="text"
+              placeholder="Zadejte GitHub uživatelské jméno"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
           <button type="submit">Vyhledat</button>
         </form>
 
         {loading ? (
           <h2 className="loading_heading">Načítání...</h2>
+        ) : error ? (
+          <h2>Uživatel {username} nenalezen.</h2>
         ) : (
           <>
             <h2>Login: {login}</h2>
